4c3cbbd1bf7408ddbb2f4fcca115a29b81d9e793
plecz <pal.lecz@gmail.com>
2018-06-24 13:06:21 +0200

    Add tests for ESPosition

---
+++ PuzzleCore.Test/ESPosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace PuzzleCore.Test
+{
+    public class ESPosition
+    {
+        #pragma warning disable xUnit1026 // Warning about unused test parameters
+        [Theory]
+        [ClassData(typeof(TD_ESPosition))]
+        public void ESPosition_ColumnCorrect(string data, int column, int row)
+        {
+            var sut = Board.FromString(data);
+
+            Assert.Equal(sut.ESPosition.Item1, column);
+        }
+
+        [Theory]
+        [ClassData(typeof(TD_ESPosition))]
+        public void ESPosition_RowCorrect(string data, int column, int row)
+        {
+            var sut = Board.FromString(data);
+
+            Assert.Equal(sut.ESPosition.Item2, row);
+        }
+        #pragma warning restore xUnit1026
+    }
+}
